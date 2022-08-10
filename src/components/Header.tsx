d2b2373b28419933ead2fa4d8463b176b78555af
Potato <poltoradnevone@gmail.com>
2022-08-10 18:02:22 +0400

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className="mt-24">
+    <div>
+        
+    </div>
+    <div>
+
+    </div>
+  </header>;
+};
+
+export default Header;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,56 @@
+import Logo from '../images/logo.svg';
+
+const Navbar = () => {
+  return (
+    <nav className="border-t-[5px] border-t-blue-600 fixed top-0 left-0 right-0 p-6 bg-white border-b border-b-gray-100 shadow">
+      <div className="w-[90%] m-auto flex justify-between items-center">
+        <div>
+          <img src={Logo} className="w-[135px]" alt="Kajabi" />
+        </div>
+        <ul className="flex items-center gap-10 text-md text-gray-500">
+          <li className="flex items-center justify-center gap-1 cursor-pointer hover:font-semibold">
+            Products
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4 text-gray-800"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </li>
+          <li className="cursor-pointer">Pricing</li>
+          <li className="flex items-center justify-center gap-1 cursor-pointer hover:font-semibold">
+            Resources{' '}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4 text-gray-800"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </li>
+          <li className="cursor-pointer">Login</li>
+          <li className="bg-blue-500 py-2 px-6 rounded-[30px] font-semibold text-white cursor-pointer hover:bg-blue-600 transition duration-200">
+            Get 3 Months for $99
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
